327c2c4d3b585e5d6f85b3f3feb4c35f9ac2b482
N1Coder <tomofanteng123@gmail.com>
2022-10-16 12:44:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted } from "vue"
+import { RouterLink, RouterView } from "vue-router"
+import HelloWorld from "./components/HelloWorld.vue"
+
+const url = "https://masak-apa.tomorisakura.vercel.app"
+
+const fetchFood = () => {
+  fetch(`${url}/api/search/?q=coto`)
+    .then((res) => console.log(res))
+    // .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+}
+</script>
+
+<template>
+  <button @click="fetchFood">get food</button>
+</template>
